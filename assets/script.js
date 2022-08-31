c1ec7023c4562b4d4e41acc21e0c55a2bd4bc290
morgan woods <morganwoods618@gmail.com>
2022-08-30 19:23:45 -0600

    more updates

--- assets/script.js
+++ assets/script.js
@@ -1,47 +1,56 @@
-$("#search-button").on("click",function(){
+$("#search-button").on("click", function () {
     var inputValue = $("#search-value").val()
-    console.log (inputValue)
+    console.log(inputValue)
     geoCode(inputValue)
 })
 
-function geoCode (cityName){
+function geoCode(cityName) {
     fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=5&appid=6ca3b3921f05e6965220c35abefb7227`)
-    .then(response=>response.json())
-    .then(data=>{
-        console.log(data)
-        getCurrentWeather(data[0].lat, data[0].lon)
-        getForecast(data[0].lat, data[0].lon)
-    })
+        .then(response => response.json())
+        .then(data => {
+            console.log(data)
+            getCurrentWeather(data[0].lat, data[0].lon)
+            getForecast(data[0].lat, data[0].lon)
+        })
 }
 
-function getCurrentWeather (lat, lon){
+//current
+function getCurrentWeather(lat, lon) {
     fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=imperial&exclude=hourly&appid=6ca3b3921f05e6965220c35abefb7227`)
-    .then(response=>response.json())
-    .then(data=>{
-        console.log(data)
-        var weatherCard = $("<div>").addClass("card")
-        var temp = $("<h3>").text("temperature; " + data.main.temp)
-        var cityName = $("<h3>").text(data.name)
-        var humidity = $("<h3>").text("humidity; " + data.main.humidity)
-        var wind = $("<h3>").text("Wind; " + data.wind.speed)
-        weatherCard.append(cityName, temp, humidity, wind)
-        $("#todayweather").append(weatherCard)
-    })
+        .then(response => response.json())
+        .then(data => {
+            console.log(data)
+            var weatherCard = $("<div>").addClass("today")
+            weatherCard.attr('class', 'today');
+            var temp = $("<h4>").text("Temperature: " + data.main.temp + "°F")
+            var cityName = $("<h3>").text(data.name)
+            var humidity = $("<h4>").text("Humidity: " + data.main.humidity + "%")
+            var wind = $("<h4>").text("Wind: " + data.wind.speed + " MPH")
+            var icon =$("<h4>").text(data.weather[0].icon)
+            var today = moment().format('L');
+            weatherCard.append(cityName, today, temp, humidity, wind, icon)
+            $("#todayweather").append(weatherCard)
+        })
 }
 
-function getForecast (lat, lon){
+
+//5 day
+function getForecast(lat, lon) {
     fetch(`http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=imperial&cnt=6&appid=6ca3b3921f05e6965220c35abefb7227`)
-    .then(response=>response.json())
-    .then(data=>{
-        console.log(data)
-        for (var i = 1; i<data.list.length; i++){
-        var weatherCard = $("<div>").addClass("card")
-        var temp = $("<p>").text("temp; " + data.list[i].main.temp)
-
-n
-
-        weatherCard.append(temp)
-        $("#fiveforecast").append(weatherCard)
-        }
-    })
+        .then(response => response.json())
+        .then(data => {
+            console.log(data)
+            for (var i = 1; i < data.list.length; i++) {
+                var weatherCard = $("<div>").addClass("card forecast")
+                var date = $("<h4>").text(data.list[i].dt_txt)
+                var temp = $("<p>").text("Temperature: " + data.list[i].main.temp + "°F")
+                var humidity = $("<p>").text("Humidity: " + data.list[i].main.humidity + "%")
+                var wind = $("<p>").text("Wind: " + data.list[i].wind.speed + " MPH")
+                weatherCard.append(date, temp, humidity, wind)
+                $("#fiveforecast").append(weatherCard)
+                // $("#Day-1").append(weatherCard[0])
+            }
+        })
 }
+
+
